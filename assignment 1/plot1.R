bfe4a691c1d71cf611e03ccf388c07eb84552601
Brad Weiner <bradweiner@gmail.com>
2014-05-08 11:56:10 -0500

    Moved into assignment 1 folder

--- assignment 1/plot1.R
+++ assignment 1/plot1.R
@@ -1,10 +1,14 @@
+library(data.table)
 elec<-fread(input="C:/Users/wein0339/Desktop/Dropbox/Coursera/Exploratory Data Analysis/household_power_consumption.txt", sep=";"
             , na.strings=c("?"))
+
+elec$Date<-as.Date(elec$Date, "%d/%m/%Y")
 
 #Subset February 1st and 2nd, 2007
 #In this file formats are dd/mm/yyyy
 #Searching for 1/2/2007 & 2/2/2007
-elec1<-subset(elec,Date=="1/2/2007" | Date=="2/2/2007")
+elec1<-subset(elec,Date=="2007-02-01" | Date=="2007-02-02")
+elec1$GAP<-as.numeric(elec1$Global_active_power)
 
 #set up plot output
 png("C:/Users/wein0339/Desktop/Dropbox/Coursera/Exploratory Data Analysis/plot1.png", width = 480, height = 480)
